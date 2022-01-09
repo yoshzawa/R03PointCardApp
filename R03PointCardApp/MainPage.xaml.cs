f2be57f68becbf00728c411428c4581d80d92d64
yoshzawa <yoshzawa@gmail.com>
2022-01-09 10:28:55 +0900

    プロジェクト ファイルを追加します。

---
+++ R03PointCardApp/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace R03PointCardApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
