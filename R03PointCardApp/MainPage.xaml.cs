4ac7d439b73daa5ece3dcd1b20e3edd0263d612d
yoshzawa <yoshzawa@gmail.com>
2022-01-09 17:23:02 +0900

    動くかな？

--- R03PointCardApp/MainPage.xaml.cs
+++ R03PointCardApp/MainPage.xaml.cs
@@ -1,18 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using R03PointCardApp.Identity;
+using System;
 using Xamarin.Forms;
+
+
 
 namespace R03PointCardApp
 {
     public partial class MainPage : ContentPage
     {
+        private readonly IMicrosoftAuthService microsoftAuthService;
+        private bool isLoading;
+        private User user;
         public MainPage()
         {
             InitializeComponent();
+            microsoftAuthService = new MicrosoftAuthService();
+
         }
+
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            this.microsoftAuthService.Initialize();
+            user = await this.microsoftAuthService.OnSignInAsync();
+
+
+        }
+
     }
 }
