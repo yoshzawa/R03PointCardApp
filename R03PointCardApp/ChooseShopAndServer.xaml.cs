dfa24c5581fd89886c632d9bcea09554e4c16891
yoshzawa <yoshzawa@gmail.com>
2022-01-13 14:46:05 +0900

    サーバーを選択できるようにした

--- R03PointCardApp/ChooseShopAndServer.xaml.cs
+++ R03PointCardApp/ChooseShopAndServer.xaml.cs
@@ -29,14 +29,14 @@ namespace R03PointCardApp
         internal void setUser(User u)
         {
             user = u;
-            title.Text = "User : " + u.DisplayName;
+            labelUser.Text = "User : " + u.DisplayName;
         }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
             Analytics.TrackEvent("Button_Clicked");
             QRScanPage qr = new QRScanPage();
-            Navigation.PushAsync(qr );
+            Navigation.PushAsync(qr);
             qr.setParent(this);
 
         }
@@ -44,27 +44,51 @@ namespace R03PointCardApp
         internal void setTENPO_NO(string text)
         {
             TENPO_NO = text;
+            labelTenpo.Text = "Tenpo : " +text;
+
         }
 
-        private object QRScanPage()
-        {
-            throw new NotImplementedException();
-        }
+
 
 
 
         private async void Button_Clicked_1(object sender, EventArgs e)
         {
-            String url = "http://192.168.54.190:8080/R03JsonKadai00/";
-            await DisplayAlert("URL" ,url + "getPoint?TENPO_ID=" + TENPO_NO + "&USER_ID=" + user.Id ,"OK");
-            HttpClient client = new HttpClient();
-            NameValueCollection queryString = HttpUtility.ParseQueryString(string.Empty);
-            Uri uri = new Uri(string.Format(url, string.Empty));
-            HttpResponseMessage response = await client.GetAsync(uri+ "getPoint?TENPO_ID=" + TENPO_NO + "&USER_ID=" + user.Id);
-            string s = await response.Content.ReadAsStringAsync();
-            await DisplayAlert("response", s, "OK");
+            {
+                string[] urls = {
+                    "http://192.168.54.190:8080/R03JsonKadai00/",
+                    "http://192.168.54.190:8080/R03JsonKadai01/",
+                    "http://192.168.54.190:8080/R03JsonKadai02/",
+                    "http://192.168.54.190:8080/R03JsonKadai03/",
+                    "http://192.168.54.190:8080/R03JsonKadai04/",
+                    "http://192.168.54.190:8080/R03JsonKadai05/",
+                    "http://192.168.54.190:8080/R03JsonKadai06/",
+                    "http://192.168.54.190:8080/R03JsonKadai07/",
+                    "http://192.168.54.190:8080/R03JsonKadai08/",
+                    "http://192.168.54.190:8080/R03JsonKadai09/",
+                    "http://192.168.54.190:8080/R03JsonKadai10/",
+                    "http://192.168.54.190:8080/R03JsonKadai11/",
+                    "http://192.168.54.190:8080/R03JsonKadai12/",
+                    "http://192.168.54.190:8080/R03JsonKadai13/",
+                };
+                String url = urls[picker.SelectedIndex];
+                await DisplayAlert("URL", url + "getPoint?TENPO_ID=" + TENPO_NO + "&USER_ID=" + user.Id, "OK");
+                HttpClient client = new HttpClient();
+                NameValueCollection queryString = HttpUtility.ParseQueryString(string.Empty);
+                Uri uri = new Uri(string.Format(url, string.Empty));
+                HttpResponseMessage response = await client.GetAsync(uri + "getPoint?TENPO_ID=" + TENPO_NO + "&USER_ID=" + user.Id);
+                if (response.StatusCode.Equals(200))
+                {
+                    string s = await response.Content.ReadAsStringAsync();
+                    await DisplayAlert("response", s, "OK");
+                }
+                else
+                {
+                    await DisplayAlert("へんじがない。ただのしかばねのようだ", "status=" + response.StatusCode.ToString(), "OK");
+                }
 
 
+            }
         }
     }
 }
